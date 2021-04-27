4e3d16a5139e47900e48bc1499033c2de5b0edb1
Md. Mahmudul Hasan Faisal <bdmhfaisal@gmail.com>
2021-04-27 13:33:52 +0600

    added extra type gaurd for enum :: DrugType mapping in backend

--- ePrescript/UserInterface/webApp/src/app/shared/drugs-details.service.ts
+++ ePrescript/UserInterface/webApp/src/app/shared/drugs-details.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { DrugsDetails } from './drugs-details.model';
 import { HttpClient } from "@angular/common/http";
+import { DrugType } from './drug-type.enum';
 
 @Injectable({
   providedIn: 'root'
@@ -10,7 +11,9 @@ export class DrugsDetailsService {
   constructor(private http: HttpClient) { }
   readonly baseUrl = "http://localhost:31257/api/Drugs";
   formData: DrugsDetails = new DrugsDetails();
+
   postDrugDetails(){
+    this.formData.drugType = parseInt(this.formData.drugType.toString());
     return this.http.post(this.baseUrl, this.formData);
   }
 }
